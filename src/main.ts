548e12d387dbb7a3ed24be5e9711279d597da9de
Vân Long <nhvlongg@gmail.com>
2019-11-30 16:28:13 +0700

    update type to make plugin works

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import Vue from 'vue';
-import OTPInput from '@/entry';
+import OTPInput from '@8bu/vue-otp-input';
 import App from './app';
 
 Vue.config.productionTip = false;
 Vue.use(OTPInput);
-console.log(OTPInput);
 new Vue({
   render: h => h(App),
 }).$mount('#app');
