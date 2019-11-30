2a29b776e83334f719d5cbe9fe4ab89490e42b6f
Vân Long <nhvlongg@gmail.com>
2019-11-30 15:44:57 +0700

    verson updating

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,9 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
-import { OTPInput8 } from '@8bu/vue-otp-input';
-import '@8bu/vue-otp-input/dist/vue-otp-input.css';
 import './app.scss';
 
 @Component({
   name: 'App',
-  components: {
-    'otp-input': OTPInput8,
-  },
 })
 export default class App extends Vue {
   protected render() {
