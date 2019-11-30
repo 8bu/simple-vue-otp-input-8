4b5de4680dcbb8975b570d517757c04be2191a65
Vân Long <nhvlongg@gmail.com>
2019-11-30 17:17:48 +0700

    update package

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
-// import OTPInput from '@8bu/vue-otp-input';
+import OTPInput from '@8bu/vue-otp-input';
 // import OTPInput from '@/entry';
 import App from './app';
 
 Vue.config.productionTip = false;
-// Vue.use(OTPInput);
+Vue.use(OTPInput);
 new Vue({
   render: h => h(App),
 }).$mount('#app');
